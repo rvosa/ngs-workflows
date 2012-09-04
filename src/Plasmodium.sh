486ec5522ef7117eb433462ff62bf03071adba5b
Darin London <darin.london@duke.edu>
2012-09-04 12:46:11 -0400

    fixed to run through with Paired End Samples.

--- src/Plasmodium.sh
+++ src/Plasmodium.sh
@@ -6,30 +6,29 @@
 #script depends on samtools and bwa, which can be downloaded and installed
 #by running 'make' in this directory.
 
-export PATH=$PATH:`pwd`/../bin/bwa:`pwd`/../bin/samtools
-
 # where we will download the data
-DATA=../data/plasmodium
+DATA=data/plasmodium
 
 # location of a 282Mb Illumina Genome Analyzer II run, PAIRED, FASTQ
-SAMPLEFILE=ERR022523_1.fastq
-SAMPLEURL=ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR022/ERR022523/${SAMPLEFILE}.gz
+SAMPLEFILE_BASE=ERR022523
+SAMPLEURL_BASE=ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR022/ERR022523
+SAMPLEFILES=""
 
 # location of a Plasmodium falciparum reference genome
 REFERENCEFILE=Plasmodium_falciparum_OLD.faa
 REFERENCEBASEURL=ftp://ftp.ncbi.nih.gov/genomes/Plasmodium_falciparum_OLD
 
 # alignment file
-ALIGNMENTSAI=aln_sa.sai
+ALIGNMENTSAIFILES=""
 ALIGNMENTSAM=aln.sam
-ALIGNMENTBAM=aln.bam
+ALIGNMENTBAM=aln
 
 # accession numbers in the reference genome. Each is one chromosome.
 ACCESSIONS="NC_004325 NC_000910 NC_000521 NC_004318 NC_004326 NC_004327 NC_004328 NC_004329 NC_004330 NC_004314 NC_004315 NC_004316 NC_004331 NC_004317"
 
 # location of the tools we will use. it is assumed they can be found on the $PATH.
-BWA=bwa
-SAMTOOLS=samtools
+BWA=`pwd`/bin/bwa
+SAMTOOLS=`pwd`/bin/samtools
 CURL=curl
 
 # make the data directory if it doesn't exist
@@ -38,22 +37,40 @@ if [ ! -d $DATA ]; then
 fi
 
 # download Illumina run (Paired FASTQ) of Plasmodium falciparum
-if [ ! -e "$DATA/$SAMPLEFILE" ]; then
+for PAIR in 1 2; do
+  SAMPLEFILE="${SAMPLEFILE_BASE}_${PAIR}.fastq"
+  ALIGNMENTFILE="${SAMPLEFILE_BASE}_${PAIR}.sai"
+  SAMPLEURL="${SAMPLEURL_BASE}/${SAMPLEFILE}.gz"
+  if [ ! -e "${DATA}/${SAMPLEFILE}" ]; then
+    echo "${CURL} ${SAMPLEURL} | gunzip > ${SAMPLEFILE}"
 	cd $DATA
-	$CURL -O $SAMPLEURL
-	gunzip "${SAMPLEFILE}.gz"
+	$CURL $SAMPLEURL | gunzip > $SAMPLEFILE
 	cd -
-fi
+  fi
+
+  if [ -z $ALIGNMENTSAIFILES ]; then
+    ALIGNMENTSAIFILES=$ALIGNMENTFILE
+  else
+    ALIGNMENTSAIFILES="${ALIGNMENTSAIFILES} ${ALIGNMENTFILE}"
+  fi
+
+  if [ -z $SAMPLEFILES ]; then
+    SAMPLEFILES=$SAMPLEFILE
+  else
+    SAMPLEFILES="${SAMPLEFILES} ${SAMPLEFILE}"
+  fi
+
+done
 
 # download reference FASTA sequences
-if [ ! -e "$DATA/$REFERENCEFILE" ]; then
+if [ ! -e "${DATA}/${REFERENCEFILE}" ]; then
 	cd $DATA
 	COUNTER=1
 	for ACCESSION in $ACCESSIONS; do
-		if [ ! -e "${ACCESSION}.faa" ]; then 
-			$CURL -O "${REFERENCEBASEURL}/CHR${COUNTER}/${ACCESSION}.faa"	
+		if [ ! -e "${ACCESSION}.faa" ]; then
+            echo "${CURL} ${REFERENCEBASEURL}/CHR${COUNTER}/${ACCESSION}.faa >> ${REFERENCEFILE}"
+			$CURL ${REFERENCEBASEURL}/CHR${COUNTER}/${ACCESSION}.faa >> ${REFERENCEFILE}
 		fi
-		cat "${ACCESSION}.faa" >> $REFERENCEFILE
 		COUNTER=$[COUNTER + 1]
 	done
 	cd -
@@ -61,28 +78,43 @@ fi
 
 # do bwa index
 if [ ! -e "$DATA/${REFERENCEFILE}.amb" ]; then
+    echo "${BWA} index ${REFERENCEFILE}"
 	cd $DATA
-	$BWA index -a bwtsw $REFERENCEFILE
+	$BWA index $REFERENCEFILE
 	cd -
 fi
 
-# do bwa aln
-if [ ! -e "$DATA/$ALIGNMENTSAI" ]; then
+# do samtools index
+if [ ! -e "${DATA}/${REFERENCEFILE}.fai" ]; then
+  echo "${SAMTOOLS} faidx ${REFERENCEFILE}"
+  cd $DATA
+  $SAMTOOLS faidx ${REFERENCEFILE}
+  cd -
+fi
+
+# do bwa aln on each PAIR separately
+for SAMPLEFILE in $SAMPLEFILES; do
+  ALIGNMENTSAI=`echo $SAMPLEFILE | sed 's/fastq/sai/'`
+  if [ ! -e "$DATA/$ALIGNMENTSAI" ]; then
+	echo "${BWA} aln ${REFERENCEFILE} ${SAMPLEFILE} > ${ALIGNMENTSAI}"
 	cd $DATA
 	$BWA aln $REFERENCEFILE $SAMPLEFILE > $ALIGNMENTSAI
 	cd -
-fi
+  fi
+done
 
-# do bwa samse. this step is giving errors, not sure what I'm doing wrong.
+# do bwa sampe on paired alignments to produce a single sam alignment
 if [ ! -e "$DATA/$ALIGNMENTSAM" ]; then
+	echo "${BWA} sampe ${REFERENCEFILE} ${ALIGNMENTSAIFILES} ${SAMPLEFILES} > ${ALIGNMENTSAM}"
 	cd $DATA
-	$BWA samse $REFERENCEFILE $ALIGNMENTSAI $SAMPLEFILE > $ALIGNMENTSAM
+	$BWA sampe $REFERENCEFILE $ALIGNMENTSAIFILES $SAMPLEFILES > $ALIGNMENTSAM
 	cd -
 fi
 
-# make bam file
-if [ ! -e "$DATA/$ALIGNMENTBAM" ]; then
+# make bam file. Note samtools sort automatically appends .bam to the filename
+if [ ! -e "${DATA}/${ALIGNMENTBAM}.bam" ]; then
+    echo "${SAMTOOLS} view -bt ${REFERENCEFILE}.fai ${ALIGNMENTSAM} | ${SAMTOOLS} sort - ${ALIGNMENTBAM}"
 	cd $DATA
-	$SAMTOOLS view -b -h $ALIGNMENTSAM > $ALIGNMENTBAM
+	$SAMTOOLS view -bt ${REFERENCEFILE}.fai $ALIGNMENTSAM | $SAMTOOLS sort - $ALIGNMENTBAM
 	cd -
 fi
