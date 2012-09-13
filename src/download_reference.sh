4f54da44f5ffae606d10af6fbc9d892ccd878c4b
Darin London <darin.london@duke.edu>
2012-09-13 10:56:25 -0400

    actually works

--- src/download_reference.sh
+++ src/download_reference.sh
@@ -4,13 +4,11 @@
 DATA=data/plasmodium
 
 # location of a Plasmodium falciparum reference genome
-REFERENCEFILE=Plasmodium_falciparum_OLD.fna
+REFERENCEFILE=${DATA}/Plasmodium_falciparum_OLD.fna
 REFERENCEBASEURL=ftp://ftp.ncbi.nih.gov/genomes/Plasmodium_falciparum_OLD
 
 # accession numbers in the reference genome. Each is one chromosome.
 ACCESSIONS="NC_004325 NC_000910 NC_000521 NC_004318 NC_004326 NC_004327 NC_004328 NC_004329 NC_004330 NC_004314 NC_004315 NC_004316 NC_004331 NC_004317"
-
-CURL=curl
 
 # make the data directory if it doesn't exist
 if [ ! -d $DATA ]; then
@@ -18,26 +16,23 @@ if [ ! -d $DATA ]; then
 fi
 
 # download reference FASTA sequences
-if [ ! -e "${DATA}/${REFERENCEFILE}" ]; then
-	cd $DATA
+if [ ! -e "${REFERENCEFILE}" ]; then
 	COUNTER=1
 	for ACCESSION in $ACCESSIONS; do
 		if [ ! -e "${ACCESSION}.fna" ]; then
             # fix the chromosome entries on the reference
-            echo "${CURL} ${REFERENCEBASEURL}/CHR${COUNTER}/${ACCESSION}.fna >> ${REFERENCEFILE}.wrong_headers"
-			$CURL ${REFERENCEBASEURL}/CHR${COUNTER}/${ACCESSION}.fna >> ${REFERENCE}.wrong_headers
+            echo "curl ${REFERENCEBASEURL}/CHR${COUNTER}/${ACCESSION}.fna >> ${REFERENCEFILE}"
+			curl ${REFERENCEBASEURL}/CHR${COUNTER}/${ACCESSION}.fna >> ${REFERENCEFILE}
 		fi
 		COUNTER=$[COUNTER + 1]
 	done
-	cd -
-fi
-
-# this perl process changes the downloaded reference fasta file.
+# this perl process changes the downloaded reference fasta file in place.
 # it simplifies the fasta headers to just contain the chromosome,
 # and nothing else this is necessary because the header value will
 # end up in the sam file for each read that maps.  If you use
 # the original fasta headers, it will introduce whitespace
 # into the sam entry that will make parsing the entry in other
 # programs impossible.
-echo "perl -pi -e 's/^\>gi.*chromosome\s(\d+).*/\>chr$1/g' >> ${REFERENCEFILE}.wrong_headers > ${REFERENCEFILE}"
-perl -pi -e 's/^\>gi.*chromosome\s(\d+).*/\>chr$1/g' >> ${REFERENCEFILE}.wrong_headers > ${REFERENCEFILE}
+  echo "perl -pi -e 's/^\>gi.*chromosome\s(\d+).*/\>chr$1/g' ${REFERENCEFILE}"
+  perl -pi -e 's/^\>gi.*chromosome\s(\d+).*/\>chr$1/g' ${REFERENCEFILE}
+fi
